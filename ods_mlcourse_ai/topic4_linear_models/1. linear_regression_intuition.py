504dfdc119ad26f79a2501eed2366926e74c016e
Artem Ryblov <ryblovartem@gmail.com>
2021-11-26 10:29:29 +0300

    adding my solution to first assignment on linear models

--- ods_mlcourse_ai/topic4_linear_models/1. linear_regression_intuition.py
+++ ods_mlcourse_ai/topic4_linear_models/1. linear_regression_intuition.py
@@ -1,12 +1,15 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso, LassoCV, RidgeCV
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+SEED = 42
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=True)
+X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+X.shape
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, shuffle=True)
 
 lr = LinearRegression(n_jobs=-1)
 lr.fit(X_train, y_train)
@@ -24,3 +27,41 @@ pred_0 = lr.predict(X_test.values[0].reshape(1, -1))[0]
 
 mse_0_by_hand = np.power(y_test.values[0] - y_pred[0], 2)
 mse_0 = mean_squared_error([y_test.values[0]], [y_pred[0]])
+
+
+lasso = Lasso(alpha=0.1)
+lasso.fit(X_train, y_train)
+
+print(lasso.intercept_)
+print(lasso.coef_)
+print(lasso.score(X_test, y_test))
+
+y_pred = lasso.predict(X_test)
+
+mse_lasso = mean_squared_error(y_true=y_test, y_pred=y_pred)
+
+num_alphas = 200
+alphas = np.linspace(0.01, 10, num_alphas)
+lasso_cv = LassoCV(alphas=alphas, cv=5, random_state=SEED)
+lasso_cv.fit(X_train, y_train)
+
+print(lasso_cv.intercept_)
+print(lasso_cv.coef_)
+print(lasso_cv.score(X_test, y_test))
+
+
+y_pred = lasso_cv.predict(X_test)
+mse_lasso_cv = mean_squared_error(y_true=y_test, y_pred=y_pred)
+
+
+n_alphas = 200
+ridge_alphas = np.logspace(-2, 6, n_alphas)
+ridge_cv = RidgeCV(alphas=ridge_alphas, scoring="neg_mean_squared_error", cv=3)
+ridge_cv.fit(X_train, y_train)
+
+print(ridge_cv.intercept_)
+print(ridge_cv.coef_)
+print(ridge_cv.score(X_test, y_test))
+
+y_pred = ridge_cv.predict(X_test)
+mse_ridge_cv = mean_squared_error(y_true=y_test, y_pred=y_pred)
