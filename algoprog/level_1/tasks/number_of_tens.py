c627c5b3095fb058f18ce36c04ccd64b84e88cce
Artem Ryblov <ryblovartem@gmail.com>
2022-01-13 19:53:20 +0300

    added new course 'algoprog'

---
+++ algoprog/level_1/tasks/number_of_tens.py
@@ -0,0 +1,8 @@
+def main():
+    n = int(input())
+
+    print((n % 100 - n % 10) // 10)
+
+
+if __name__ == '__main__':
+    main()
---
+++ algoprog/level_1/tasks/number_of_tens_for_two_digit_number.py
@@ -0,0 +1,8 @@
+def main():
+    n = int(input())
+
+    print((n - n % 10) // 10)
+
+
+if __name__ == '__main__':
+    main()
